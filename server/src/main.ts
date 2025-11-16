61453364a38c73b2fc95ebbcc7ac68b65a2e13f1
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2025-11-16 16:55:07 +0200

    Global '/api' route prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set global path - there are options, for example, to exclude certain paths
+  app.setGlobalPrefix("api");
+
   await app.listen(process.env.PORT || 3000);
 }
 
